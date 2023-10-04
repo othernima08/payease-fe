9637348437b04bb1d1cf732ac54c704ec9f636ba
Desi Christine Othernima <desichristine2@gmail.com>
2023-10-04 17:53:15 +0700

    fix hover and active in menu item sidebar

--- src/components/sidebarMenuItem/index.jsx
+++ src/components/sidebarMenuItem/index.jsx
@@ -8,7 +8,7 @@ function MenuItem(props) {
     return (
         <section style={{display:'flex', alignItems:'center'}} key={menu.index} onClick={handleClick}>
             <section className="active-indicator" style={{display: selectedIndex === menu.index ? "block" : "none"}}></section>
-            <section className='sidebar-menu-container' style={{color: selectedIndex === menu.index ?  '#6379F4' :'#626468'}} >
+            <section className={selectedIndex === menu.index ? 'active-sidebar-menu-container' : 'sidebar-menu-container'} >
                 <p className="menu-icon">
                     {menu.icon}
                 </p>
