f3ddd65026bb19e43f1e3dc01e6034ca4a3389c3
Desi Christine Othernima <desichristine2@gmail.com>
2023-10-04 17:23:06 +0700

    fix layout

--- src/layout/afterLogin/index.jsx
+++ src/layout/afterLogin/index.jsx
@@ -25,10 +25,10 @@ const AfterLoginLayout = (props) => {
         <Container bsPrefix="after-login-container">
             <CustomNavbar user={user} />
             <Row style={{ margin: "32px 72px" }}>
-                <Col xs={4}>
+                <Col xs={12} md={4}>
                     <CustomSidebar />
                 </Col>
-                <Col xs={8}>
+                <Col xs={12} md={4}>
                     {children}
                 </Col>
             </Row>
