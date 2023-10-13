3741cf2a8b3477992ce98973a0e9dd929bb7bbda
yopijunita <yopiambarita06@gmail.com>
2023-10-13 18:45:17 +0700

    payment-method

---
+++ src/components/reusable-components/grafik/index.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { Row, Col, Card } from "react-bootstrap";
+import {
+  BarElement,
+  CategoryScale,
+  Chart,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from "chart.js";
+
+const Grafik = () => {
+  
+  Chart.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Grafik Pemasukan dan Pengeluaran",
+      },
+    },
+    scales: {
+      y: {
+        display: false,
+      },
+      x: {
+        grid: {
+          display: false,
+          drawBorder: false,
+        },
+      },
+    },
+  };
+
+  const chartLabels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+
+  const chartData = {
+    labels: chartLabels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [900, 590, 150, 280, 890, 570, 200],
+        backgroundColor: "#6379F4",
+        borderRadius: 100,
+        barPercentage: 0.2,
+        borderSkipped: false,
+      },
+    ],
+  };
+  return (
+    <Row>
+      <Col md={12}>
+        <Card className="chart-card">
+          <Card.Body>
+            <div className="chart-container">
+              <Bar options={chartOptions} data={chartData} />
+            </div>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+
+export default Grafik;
--- src/services/transactions.js
+++ src/services/transactions.js
@@ -1,3 +1,41 @@
-import { fetchApi } from '../config/api'
+import { fetchApi } from "../config/api";
 
 const BASE_URL_API_DEV = import.meta.env.VITE_API_URL_DEV;
+
+export const getVirtualAccountByUserId = async (userId) => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const url = `${BASE_URL_API_DEV}/virtual-accounts/${userId}`;
+    const response = await fetchApi({
+      url,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getVirtualAccountById = async (virtualAccountId) => {
+    try {
+      const token = localStorage.getItem("token");
+  
+      const url = `${BASE_URL_API_DEV}/virtual-accounts/virtual-account/${virtualAccountId}`;
+      const response = await fetchApi({
+        url,
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+  
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  };
