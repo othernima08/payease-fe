7fe0f8ff43b0d2d509666c4c8c05dbe3ae7944fd
Angga Kahaerul <anggakahaerul@upi.edu>
2023-10-14 16:49:07 +0700

    fix receiver width image

--- src/components/transferComponents/receiverCard/index.jsx
+++ src/components/transferComponents/receiverCard/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import "./receiverCard.css"
 
 const ReceiverCard = (props) => {
     const {id,firstName, lastName, phoneNumber, profilePicture} = props;
     return (
          <Link to={`../transfer/to/${id}`} >
-        <div className="card-container mb-2">
-            <div className="d-flex flex-row">
-                <div className='mx-1'>
-                    <img style={{width:"10%"}} src={profilePicture} alt="" />
+        <div className="card-container  justify-content-start mb-2">
+            <div className="d-flex flex-row justify-content-start align-items-center ">
+                <div className='d-flex flex-row mx-1 image-size'>
+                    <img className='image-size' src={profilePicture} alt="" />
                 </div>
                 <div className='d-flex flex-column p-2'>
                     <div>{firstName + ' ' + lastName}</div>
